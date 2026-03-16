67141bc2f58db11f682809ac714477319ecfd1bb
Roger Krishna <rogerkrish0005@gmail.com>
2026-03-16 21:04:25 +0530

    Updated BCNI loader for all samples

--- src/data/bnci_loader.py
+++ src/data/bnci_loader.py
@@ -8,3 +8,13 @@ def load_bnci_processed(path: Path):
     y = d["y"].astype(np.int64)
     meta = d["meta"].item()
     return X, y, meta
+
+
+def load_bnci_all_subjects(path: Path):
+
+    d = np.load(path, allow_pickle=True)
+    X           = d["X"].astype(np.float32)
+    y           = d["y"].astype(np.int64)
+    subject_ids = d["subject_ids"].astype(np.int64)
+    meta        = d["meta"].item()
+    return X, y, subject_ids, meta
